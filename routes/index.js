2ed451e45e1efab03ddf4dca6516621fb30ba795
Ben Ebel <bge1234@gmail.com>
2016-05-25 16:55:06 -0400

    finished navbar

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Accurate Door Systems' });
+});
+
+router.get('/residential', function(req, res, next) {
+  res.render('index', { title: 'Residential' });
+});
+
+router.get('/commercial', function(req, res, next) {
+  res.render('index', { title: 'Commercial' });
+});
+
+router.get('/partsandservice', function(req, res, next) {
+  res.render('index', { title: 'Parts & Service' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
